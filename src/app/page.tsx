39f769fd5de48a03c6df7d493832c589ad932f41
nathanielfostanperry <nate@ocupop.com>
2025-03-06 21:53:41 -0600

    add corners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import DemoCard from '@/components/demo/DemoCard';
 import DemoToast from '@/components/demo/DemoToast';
 import DemoDropdown from '@/components/demo/DemoDropdown';
 import DemoTabs from '@/components/demo/DemoTabs';
+import Corners from '@/components/Corners/index';
 
 export default function Home() {
   const { primaryColorScale, accentColorScale, neutralColorScale } =
@@ -17,6 +18,7 @@ export default function Home() {
     <main className="min-h-screen">
       <div className="grid md:grid-cols-2 gap-2 p-2">
         <div className="bg-red-50 rounded-lg p-8">
+          <Corners />
           <ColorSystemBuilder />
         </div>
         <div className="p-8">
